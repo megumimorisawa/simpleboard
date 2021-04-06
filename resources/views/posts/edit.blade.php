cb147e653db5be640980737f66dd0a94f918e237
EC2 Default User <ec2-user@ip-172-31-26-196.ap-northeast-1.compute.internal>
2021-04-06 18:38:57 +0900

    set validation

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,16 +4,25 @@
 
 @section('content')
     <h1>Editing Post</h1>
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form method="post" action="/posts/{{ $post->id }}">
         {{ csrf_field() }}
         <input type="hidden" name="_method" value="PUT">
         <div class="form-group">
             <label for="examoleInputEmail1">Title</label>
-            <input type="text" class="form-control" area-describedby="emailHelp" name="title" value="{{ $post->title }}">
+            <input type="text" class="form-control" area-describedby="emailHelp" name="title" value="{{ old('title') == '' ? $post->title : old('title') }}">
         </div>
         <div class="form-group">
             <label for="examoleInputPassword1">Content</label>
-            <textarea class="form-control" name="content">{{ $post->content }}</textarea>
+            <textarea class="form-control" name="content">{{ old('content') == '' ? $post->content : old('content') }}</textarea>
         </div>
         <button type="submit" class="btn btn-outline-primary">Submit</button>
         <input type="submit">
