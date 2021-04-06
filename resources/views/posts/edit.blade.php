3d0847b686c6e283bb3f352496dd6d9089c0467c
EC2 Default User <ec2-user@ip-172-31-26-196.ap-northeast-1.compute.internal>
2021-04-06 18:27:09 +0900

    fix posts layouts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,11 +3,22 @@
 @section('title', 'Simple Board')
 
 @section('content')
+    <h1>Editing Post</h1>
     <form method="post" action="/posts/{{ $post->id }}">
         {{ csrf_field() }}
         <input type="hidden" name="_method" value="PUT">
-        <input type="text" name="title" value="{{ $post->title }}">
-        <input type="text" name="content" value="{{ $post->content }}">
+        <div class="form-group">
+            <label for="examoleInputEmail1">Title</label>
+            <input type="text" class="form-control" area-describedby="emailHelp" name="title" value="{{ $post->title }}">
+        </div>
+        <div class="form-group">
+            <label for="examoleInputPassword1">Content</label>
+            <textarea class="form-control" name="content">{{ $post->content }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-outline-primary">Submit</button>
         <input type="submit">
     </form>
+    
+    <a href="/posts/{{ $post->id }}">Show</a>
+    <a href="/posts">Back</a>
 @endsection
